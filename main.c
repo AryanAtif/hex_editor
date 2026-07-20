51a6077f8e8f6aa09033234dea80775a19641dd6
AryanAtif <aryanatif01@gmail.com>
2026-07-20 20:41:55 +0500

    feat: make each hex value at least 4 characters long

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define STARTING_BUF_SIZE 1024 
-
 uint8_t *raw_file_data; 
 
 void read_file (FILE *file);
 int get_file_size (FILE *file);
+void print_hex (FILE *file);
 
 int main(int argc, char** argv)
 { 
@@ -20,25 +19,10 @@ int main(int argc, char** argv)
   else {printf ("Usage ./<output_filename> <file_to_read>\n"); return 1;}
 
   read_file (file);
+  print_hex (file);
   
   fclose(file);
   return 0;
-}
-
-
-void read_file (FILE *file)
-{
-  int file_size = get_file_size(file);
-  
-  raw_file_data = (uint8_t *) malloc (file_size * sizeof (uint8_t));
-  fread (raw_file_data, sizeof (uint8_t), file_size, file);
-
-  int i = 0;
-  while (i < 1024)
-  {
-    printf ("%X%X ", raw_file_data[i],  raw_file_data[i+1]);
-    i += 2;
-  }
 }
 
 int get_file_size (FILE *file)
@@ -48,4 +32,22 @@ int get_file_size (FILE *file)
   fseek (file, 0, SEEK_SET);
 
   return file_size;
+}
+
+void read_file (FILE *file)
+{
+  int file_size = get_file_size(file);
+  
+  raw_file_data = (uint8_t *) malloc (file_size * sizeof (uint8_t));
+  fread (raw_file_data, sizeof (uint8_t), file_size, file);
+}
+
+void print_hex (FILE *file)
+{
+  int i = 0;
+  while (i < get_file_size(file))
+  {
+    printf ("%02X%02X ", raw_file_data[i],  raw_file_data[i+1]);
+    i += 2;
+  }
 }
